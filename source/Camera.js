e3632bfa7293ab909a55f7f37edf53887561132a
Jenny Zeng <jzeng7077@gmail.com>
2021-01-28 16:43:56 -0500

    Chore: added camera

---
+++ source/Camera.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const [startCamera, setStartCamera] = React.useState(false)
+export default function Camera () {
+
+
+}
