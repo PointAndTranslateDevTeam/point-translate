dd1a40ba89cfc839621af0438f5ab044f689fef4
Frankie <fmgoodwiniv@gmail.com>
2021-01-28 20:32:02 -0500

    clean: removed unnecessary imports

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import CameraScreen from './src/screens/CameraScreen'
 
 export default function App() {
