257e7b8a79f035607a3adbd9b75c238ef5eef58a
Jenny Zeng <jzeng7077@gmail.com>
2021-01-29 18:15:05 -0500

    Added OCR feature

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,7 +1,7 @@
-import {Camera} from 'expo-camera'
+import { Camera } from "expo-camera";
 
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 //Choosing a functional component gives us access to useState hook
 const CameraScreen = () => {
@@ -9,23 +9,66 @@ const CameraScreen = () => {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [camera, setCamera] = useState(null);
   //the image should be accessible on state to any component which imports it
-  const [image, setImage] = useState(null);
+  const [picture, setPicture] = useState(null);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
   }, []);
 
   const takePicture = async () => {
-      const option = {base64: true}
-      if (camera) {
-          const data = await camera.takePictureAsync(option);
-          console.log(data);
-          setImage(data.uri);
-      }
-  }
+    const option = { base64: true };
+    if (camera) {
+      const data = await camera.takePictureAsync(option);
+      // console.log(data.base64);
+      setPicture(data.base64);
+      console.log("PICTURE", picture[1]);
+      toTest();
+    }
+    // console.log(picture)
+  };
+
+  const toTest = async () => {
+    console.log("hey");
+    try {
+      let response = await fetch(
+        "https://vision.googleapis.com/v1/images:annotate?key=" +
+        process.env.API_KEY,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            requests: [
+              {
+                image: {
+                  content: picture,
+                },
+                features: [
+                  {
+                    type: "TEXT_DETECTION",
+                  },
+                ],
+              },
+            ],
+          }),
+        }
+      );
+      console.log(picture[1]);
+      const responseJSON = await response.json();
+      // console.log(await response.json())
+      // console.log(responseJSON)
+      // console.log(responseJSON.fullTextAnnotation.text)
+
+      console.log(responseJSON.responses[0].fullTextAnnotation.text);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   if (hasPermission === null) {
     return <View />;
@@ -35,14 +78,9 @@ const CameraScreen = () => {
   }
   return (
     <View style={styles.container}>
-      <Camera 
-        ref={ref => setCamera(ref)} 
-        style={styles.camera} 
-        type={type}>
+      <Camera ref={(ref) => setCamera(ref)} style={styles.camera} type={type}>
         <View style={styles.buttonContainer}>
-            <TouchableOpacity
-            style={styles.button}
-            onPress = { () => takePicture()} >
+          <TouchableOpacity style={styles.button} onPress={() => takePicture()}>
             <Text style={styles.text}>Take Picture</Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -53,16 +91,17 @@ const CameraScreen = () => {
                   ? Camera.Constants.Type.front
                   : Camera.Constants.Type.back
               );
-            }}>
+            }}
+          >
             <Text style={styles.text}> Flip </Text>
           </TouchableOpacity>
           {/* this can be deleted once you confirm you have access to the image in api */}
-          {image && <Image source = {{uri: image}} style={{ flex: 1 }} /> }
+          {/* {image && <Image source = {{uri: image}} style={{ flex: 1 }} /> } */}
         </View>
       </Camera>
     </View>
   );
-}
+};
 
 export default CameraScreen;
 
@@ -75,17 +114,17 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
+    backgroundColor: "transparent",
+    flexDirection: "row",
     margin: 20,
   },
   button: {
     flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    alignSelf: "flex-end",
+    alignItems: "center",
   },
   text: {
     fontSize: 18,
-    color: 'white',
+    color: "white",
   },
 });
