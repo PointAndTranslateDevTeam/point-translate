eb6246dd06389f3a033a62553675156d60371257
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-01-29 15:17:09 -0500

    try 2 at fixing

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import {  Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Button
-        title="Tap to open camera"
-        onPress={() => navigation.navigate("Camera")}
-      />
+    <View style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#fff",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "#14274e",
+            fontWeight: "bold",
+            textAlign: "center",
+            fontSize: 36,
+          }}
+        >
+          Point & Translate
+        </Text>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Camera")}
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: "#14274e",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            height: 40,
+          }}
+        >
+          <Text
+            style={{
+              color: "#fff",
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            Tap For Camera
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default HomeScreen;
