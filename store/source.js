4ef8401864894fa201fa7d0e5725838a7031d84b
Svetlana Leonova <leonova_svetlana@yahoo.com>
2021-01-31 16:20:07 -0500

    redux WIP

--- store/source.js
+++ store/source.js
@@ -11,13 +11,57 @@ export const detectedText = (source) => {
   };
 };
 
+export const getText = (picture) => {
+  return async (dispatch) => {
+    try {
+      let response = await fetch(
+        "https://vision.googleapis.com/v1/images:annotate?key=" + API_KEY,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            requests: [
+              {
+                image: {
+                  content: picture,
+                },
+                features: [
+                  {
+                    type: "DOCUMENT_TEXT_DETECTION",
+                  },
+                ],
+              },
+            ],
+          }),
+        }
+      );
+      const responseJSON = await response.json();
+      if (
+        responseJSON.responses[0] === {} ||
+        !responseJSON.responses[0].fullTextAnnotation
+      ) {
+        return "Sorry, we did not detect any text in your image.";
+      } else {
+        dispatch(
+          detectedText(responseJSON.responses[0].fullTextAnnotation.text)
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
 const sourceReducer = (state = initialState, action) => {
   switch (action.type) {
     case DETECTED_TEXT:
       return {
         ...state,
         //check what recognizedText is
-        detectedText: action.source.recognizedText,
+        detectedText: action.source,
       };
     default:
       return state;
