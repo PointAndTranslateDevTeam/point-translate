da359f9066133fa60f1951e3c5adf64def6bdbf3
Frankie <fmgoodwiniv@gmail.com>
2021-02-04 17:47:56 -0500

    feat: loading wheel component (only renders if animation is set to slide atm)

--- components/LoadingWheel.js
+++ components/LoadingWheel.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { StyleSheet, View, Text, Modal, ActivityIndicator } from 'react-native';
 
 const LoadingWheel = (props) => {
     return (
-        <Modal transparent={true} visible={props.showLoading} animationType="slide">
-            <View>
-                <Text>Loading...</Text>
-            </View>
+        <Modal transparent={true} visible={props.showLoading} animationType="slide" style={[styles.container, styles.horizontal]}>
+            <View> 
+                <ActivityIndicator hidesWhenStopped={false} size="large" color="white" animating={true} style={styles.activityIndicator}/>
+            </View>   
         </Modal>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10,
+    },
+    loadingText: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        position: 'absolute',
+    }
+})
 
 export default LoadingWheel;
