6db0cda1cb244a9fc1490e650e288768002ca6a9
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-02-05 13:16:24 -0500

    styled settings container

--- components/Settings.js
+++ components/Settings.js
@@ -1,44 +1,38 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { toggleOCR } from "../store/toggleReducer";
-import styles from '../styles/SettingsStyle'
+import styles from "../styles/SettingsStyle";
 
-import {
-  View,
-  Text,
-  Modal,
-  TouchableOpacity,
-  Switch,
-} from "react-native";
+import { View, Text, Modal, TouchableOpacity, Switch } from "react-native";
 
 const Settings = (props) => {
   return (
     <Modal transparent={true} visible={props.showModal} animationType="slide">
-    <View style={styles.screenContainer}>
-      <View style={styles.screen}>
-        <View style={styles.settingContainer}>
-          <View style={styles.toggleContainer}>
-            <Text>Optimize for handwriting recognition?</Text>
-            <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={props.handwriting ? "#f5dd4b" : "#f4f3f4"}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={props.toggleOCR}
-              value={props.handwriting}
-            />
-          </View>
-          <View>
-            <TouchableOpacity onPress={() => props.setModal(false)}>
-              <Text>Back to camera</Text>
-            </TouchableOpacity>
-</View>
+      <View style={styles.screenContainer}>
+        <View style={styles.screen}>
+          <View style={styles.settingContainer}>
+            <Text style= {{color: "white", fontSize: 18, padding:15}}>Optimize for handwriting recognition?</Text>
+            <View style={styles.toggleContainer}>
+              <Switch
+                trackColor={{ false: "#006575", true: "#006575"}}
+                thumbColor={props.handwriting ? "white" : "#f4f3f4"}
+                onValueChange={props.toggleOCR}
+                value={props.handwriting}
+              />
+            </View>
+            <View style={styles.button}>
+              <TouchableOpacity onPress={() => props.setModal(false)}>
+                <Text style={{ fontWeight: "bold", color: "white" }}>
+                  Back to camera
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
     </Modal>
   );
 };
-
 
 const mapStateToProps = (state) => {
   return {
