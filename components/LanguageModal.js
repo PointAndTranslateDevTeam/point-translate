4003cc459609b531aa1b72b4eaabe828e8beaa10
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-02-04 22:50:39 -0500

    Made a modal with language picker on it. Can't exit modal, and it's not centered.

--- components/LanguageModal.js
+++ components/LanguageModal.js
@@ -2,21 +2,32 @@ import React from "react";
 import TargetPicker from "../components/TargetPicker";
 import styles from "../styles/HomeStyle";
 
-import {
-  View,
-  Text,
-  Modal,
-  TouchableOpacity,
-  Switch,
-} from "react-native";
+import { View, Text, Modal, TouchableOpacity, Switch, StyleSheet } from "react-native";
 
 const LanguageModal = (props) => {
   return (
     <Modal transparent={true} visible={props.showModal} animationType="slide">
-      <Text>Please select your target language:</Text>
+      <View style={langModStyles.container}>
+        <View style={langModStyles.pickerContainer}>
+          <Text>Please select your target language:</Text>
           <TargetPicker initialValue="es" style={styles.targetPicker} />
+        </View>
+      </View>
     </Modal>
-  )
-}
+  );
+};
 
+const langModStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#006575",
+  },
+  pickerContainer: {
+    height: 200,
+    width: "100%",
+    backgroundColor: "#2286A2",
+  },
+});
 export default LanguageModal;
