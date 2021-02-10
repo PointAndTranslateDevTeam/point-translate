45938079ce858b61563f996cf84a846100be999d
Frankie <fmgoodwiniv@gmail.com>
2021-02-10 15:28:43 -0500

    feat: audio now stops on second tap, icon changes to volume-x when sound is playing

--- components/audioButton.js
+++ components/audioButton.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import * as Speech from 'expo-speech';
 
 const AudioButton = (props) => {
+
+  const [speaking, setSpeaking] = useState(false);
+
+  const playSound = async () => {
+    { 
+      if (await Speech.isSpeakingAsync()) {
+        Speech.stop();
+        setSpeaking(false);
+      } else {
+      Speech.speak(props.text, {
+          language: props.lang
+        })
+        setSpeaking(true);
+      }
+      
+    } 
+  }
+
   return (
     <TouchableOpacity
-      onPress={() => {
-          Speech.speak(props.text, {
-            language: props.lang
-          })
-        } 
-      }
+      onPress={async () => playSound()}
       style={styles.audioContainer}
     >
       <Feather
-        name="volume-2"
+        name={speaking ? "volume-x" : "volume-2"}
         size={34}
         color="#FC9E9C"
         style={styles.outerCircle}
