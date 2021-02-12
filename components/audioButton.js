464c6f4bea28568d825922b9f57cf2197a3d235b
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-02-11 19:36:20 -0500

    fixed the buttonsss

--- components/audioButton.js
+++ components/audioButton.js
@@ -1,31 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import * as Speech from 'expo-speech';
+import * as Speech from "expo-speech";
 
 const AudioButton = (props) => {
-
   const [speaking, setSpeaking] = useState(false);
 
   const playSound = async () => {
-    { 
+    {
       if (await Speech.isSpeakingAsync()) {
         Speech.stop();
         setSpeaking(false);
       } else {
-      Speech.speak(props.text, {
-          language: props.lang
-        })
+        Speech.speak(props.text, {
+          language: props.lang,
+          onStart: () => {
+            setSpeaking(true);
+          },
+          onDone: () => {
+            setSpeaking(false);
+          },
+        });
         setSpeaking(true);
       }
-      
-    } 
-  }
+    }
+  };
 
   return (
-    <TouchableOpacity
-      onPress={async () => playSound()}
-    >
+    <TouchableOpacity onPress={async () => playSound()}>
       <Feather
         name={speaking ? "volume-x" : "volume-2"}
         size={34}
@@ -34,6 +36,5 @@ const AudioButton = (props) => {
     </TouchableOpacity>
   );
 };
-
 
 export default AudioButton;
