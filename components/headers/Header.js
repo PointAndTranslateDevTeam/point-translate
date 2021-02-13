1b192cc6e2017aa5e2217aeece047e2610354ac8
Abbie Stauffer <AbigailStauffer@gmail.com>
2021-02-12 22:45:36 -0500

    changed head title color

--- components/headers/Header.js
+++ components/headers/Header.js
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
   },
   headerTitle: {
-    color: "white",
+    color: "#FED8D7",
     fontSize: 30,
     fontWeight: "bold",
     width: "100%",
--- components/headers/TranslateHeader.js
+++ components/headers/TranslateHeader.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerTitle: {
-    color: "white",
+    color: "#FED8D7",
     fontSize: 30,
     fontWeight: "bold",
     width: "100%",
